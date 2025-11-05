5eccf61d0eae2ea587517691d7212600e96c4b68
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2025-11-05 00:19:36 -0300

    chore: Setup inicial do projeto / cursos-em-videos.

---
+++ server.js
@@ -0,0 +1,7 @@
+import app from "./src/app.js";
+
+const PORT = 7474;
+
+app.listen(PORT, () => {
+  console.log(`Servidor subiu na porta ${PORT} 🚀🚀🚀`);
+});
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/teste", (req, res) => {
+  res.status(200).send({ mensagem: "boas-vindas à API" });
+});
+
+export default app;
