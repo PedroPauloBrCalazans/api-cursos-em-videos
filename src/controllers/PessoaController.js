01b756f14ed277c26efa67e56fab6fe5b0759cf9
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2025-11-17 21:01:35 -0300

    feat: Add escopos de associação + nova rota de pessoa/estudante/matriculas ativas e todos.

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -8,12 +8,26 @@ class PessoaController extends Controller {
     super(pessoaService);
   }
 
-  async pegaMatricula(req, res) {
+  async pegaMatriculasAtivas(req, res) {
     const { estudanteId } = req.params;
     try {
-      const listaMatriculas = await pessoaService.pegaMatriculasPorEstudante(
-        Number(estudanteId)
-      );
+      const listaMatriculas =
+        await pessoaService.pegaMatriculasAtivasPorEstudante(
+          Number(estudanteId)
+        );
+      return res.status(200).json(listaMatriculas);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
+  }
+
+  async pegaTodasAsMatriculas(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      const listaMatriculas =
+        await pessoaService.pegaTodasAsMatriculasPorEstudante(
+          Number(estudanteId)
+        );
       return res.status(200).json(listaMatriculas);
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -8,22 +8,33 @@ const matriculaController = new MatriculaController();
 const router = express.Router();
 
 router.get("/pessoas", (req, res) => pessoaController.getAll(req, res));
+
 router.get("/pessoas/todos", (req, res) =>
   pessoaController.pegaTodasAsPessoas(req, res)
 );
+
 router.get("/pessoas/:id", (req, res) => pessoaController.getPorId(req, res));
+
 router.post("/pessoas", (req, res) =>
   pessoaController.cadastrarRegistro(req, res)
 );
+
 router.put("/pessoas/:id", (req, res) =>
   pessoaController.atualizarRegistro(req, res)
 );
+
 router.delete("/pessoas/:id", (req, res) =>
   pessoaController.excluirRegistros(req, res)
 );
+
 router.get("/pessoas/:estudanteId/matriculas", (req, res) =>
-  pessoaController.pegaMatricula(req, res)
+  pessoaController.pegaMatriculasAtivas(req, res)
 );
+
+router.get("/pessoas/:estudanteId/matriculas/todos", (req, res) =>
+  pessoaController.pegaTodasAsMatriculas(req, res)
+);
+
 router.post("/pessoas/:estudanteId/matriculas", (req, res) =>
   matriculaController.cadastrarRegistro(req, res)
 );
